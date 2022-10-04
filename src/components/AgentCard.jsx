c16cc98ac626f41ffacbc18fac6ce52cf75e6264
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2022-10-03 23:58:36 -0500

    avance audio

--- src/components/AgentCard.jsx
+++ src/components/AgentCard.jsx
@@ -1,54 +1,66 @@
+import "./styles/AgentCard.css";
+import ReactAudioPlayer from "react-audio-player";
 
-import './styles/AgentCard.css'
-
-function AgentCard({agent}) {
-
-  return (
-
+function AgentCard({ agent }) {
     
-    <div className='col-md-4'>
-        
-    <div class="card-list">
-
-        <div class="card h-100 mb-4">
-
-            <div class="card-image">
-                <img src={agent.fullPortrait} alt="agente"/>
-            </div>
-            <div class="card-body">
-                <span class="date">NOMBRE DE AGENTE</span>
+    /* function changeBackground(e) {
+        e.target.style.background = `linear-gradient(
+            ${agent.backgroundGradientColors[0]},
+             ${agent.backgroundGradientColors[1]},
+             ${agent.backgroundGradientColors[2]},
+             ${agent.backgroundGradientColors[3]});`;
+      } */
+     
+  {
+    if (agent.isPlayableCharacter == true) {
+      return (
+        <div class="col-md-4">
+          <div  class="card-list">
+            <div   class="card h-100 mb-4" >
+              <div class="card-image">
+                <img src={agent.fullPortrait} alt="agente" />
+              </div>
+              <div class="card-body">
+                <span class="date">Agent's Name</span>
                 <h2>{agent.displayName}</h2>
-                <p>{agent.description}</p>
+                <span>{agent.role.displayName}</span>
+                <hr></hr>
+                
+                <p>{agent.description.substring(0, 250)}</p>
+                <div id="voice">
+                <label id="voice-label">Voice:</label>
+                <ReactAudioPlayer className="audio" src={agent.voiceLine.mediaList[0].wave}  controls />
+                </div>
+              </div>
+
+              <div class="card-footer">
+                <div class="info">
+                  <div class="value">Q</div>
+                  <div class="type">{agent.abilities[0].displayName}</div>
+                </div>
+
+                <div class="info">
+                  <div class="value">E</div>
+                  <div class="type">{agent.abilities[1].displayName}</div>
+                </div>
+
+                <div class="info">
+                  <div class="value">C</div>
+                  <div class="type">{agent.abilities[2].displayName}</div>
+                </div>
+                <div class="info">
+                  <div class="value">R</div>
+                  <div class="type">{agent.abilities[3].displayName}</div>
+                </div>
+              </div>
             </div>
-
-            <div class="card-footer">
-                <div class="info">
-                    <div class="value">Q</div>
-                    <div class="type">{agent.abilities[0].displayName}</div>
-                </div>
-
-                <div class="info">
-                    <div class="value">E</div>
-                    <div class="type">{agent.abilities[1].displayName}</div>
-                </div>
-
-                <div class="info">
-                    <div class="value">C</div>
-                    <div class="type">
-                    {agent.abilities[2].displayName}
-                    </div>
-                </div>
-                <div class="info">
-                    <div class="value">R</div>
-                    <div class="type">
-                    {agent.abilities[3].displayName}
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-    </div>
-  )
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
-export default AgentCard
+export default AgentCard;
